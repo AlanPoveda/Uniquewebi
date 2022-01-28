890c80598be45788e19c2d34cd569aeb690d523d
Alan Poveda <alsepoveda@gmail.com>
2022-01-28 17:17:30 -0300

    add Haderstyle and button component style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,19 @@
-import styles from "./styles.module.scss";
+import styles from './styles.module.scss'
+import { HeaderButton } from '../HeaderButton'
 
-export function Header() {
-    return (
-        <>
-            <nav className={styles.navbars}>
-                <div>
-                    <span>Uniquewebi</span>
-                </div>
-
-                <div className={styles.links}>
-                    <a>Home</a> <a>About Us</a> <a>Services</a> <a>News</a>
-                    <button>Contact Us</button>
-                </div>
-            </nav>
-        </>
+export function Header(){
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <h1>Uniquewebi</h1>
+                <nav>
+                    <a className={styles.active}href="">Home</a>
+                    <a href="">About us</a>
+                    <a href="">Services</a>
+                    <a href="">News</a>
+                    <HeaderButton />
+                </nav>
+            </div>
+        </header>
     );
 }
---
+++ src/components/HeaderButton/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss'
+
+export function HeaderButton(){
+    return(
+        <>
+        <button className={styles.buttonHeader}>Contact Us</button>
+        </>
+    );
+
+}
