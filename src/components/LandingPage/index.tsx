74a4935f4d53d71cfaf38f3efac4090e685926f6
Alan Poveda <alsepoveda@gmail.com>
2022-02-24 15:07:47 -0300

    Upload index

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,18 +1,23 @@
-import styles from './styles.module.scss'
-
+import styles from "./styles.module.scss";
+import Image from "next/image";
+import FirstAvatar from "../../../public/images/alan.png";
 
 export const LandingPage = () => {
     return (
-        <div className={styles.container}> 
-            <h1>
-                Advance software, advanced research for <strong>simple</strong>{" "}
-                life
-            </h1>
-            <p>
-                We harness the lasted technologies to develop cutting-edge
-                digital solutions for modern business requirements
-            </p>
-            <a>Get a Quote <button>{'->'}</button></a>
-        </div>
+        <>
+            <div className={styles.container}>
+                <h1>
+                    Advance software, advanced research for{" "}
+                    <strong>simple</strong> life
+                </h1>
+                <p>
+                    We harness the lasted technologies to develop cutting-edge
+                    digital solutions for modern business requirements
+                </p>
+                <a>
+                    Get a Quote <button>{"->"}</button>
+                </a>
+            </div>
+        </>
     );
 };
